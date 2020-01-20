45e11cc57f4b57b997ebd8440df57ba0d15b41a0
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-01-20 00:55:48 -0300

    bug fix

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ To start the project use docker-compose up -d --build
 
 [1] create a user /register
 [2] create a store of user
-[3] tensorflow inception v3 > google 
+[3] tensorflow inception v3 > google
+
+obs:::  configurar tox , criar testes unitario -> pytest,  criar schema apis ( marshmallow ), criar fuxos dos serviços.
--- web/app.py
+++ web/app.py
@@ -6,7 +6,6 @@ from bottle import Bottle
 import json, subprocess
 from json import dumps
 
-
 # class DemoResource(BottleResource):
 #
 #     @api('/demos')
@@ -35,45 +34,42 @@ from json import dumps
 
 
 # instance of mongodb
-# client = MongoClient("mongodb://mongo:27017")
-# db = client.dockerDB
-# user_num = db["user_num"]     # instance of collections one
-# user_num.insert({
-#     "num_of_users": 0
-# })
-#
-# db = client.sentence_database
-# users = db["users"]     # instance of collections two
-#
-# db = client.image_recognition
-# users_recognition = db["users_recognition"]     # instance of collections two
+client = MongoClient("mongodb://mongo:27017")
+db = client.dockerDB
+user_num = db["user_num"]  # instance of collections one
+user_num.insert({
+    "num_of_users": 0
+})
+
+db = client.sentence_database
+users = db["users"]  # instance of collections two
+
+db = client.image_recognition
+users_recognition = db["users_recognition"]  # instance of collections two
+
 
 # https://passlib.readthedocs.io/en/stable/lib/passlib.hash.bcrypt.html
-
 class Register(BottleResource):
 
     @api('/')
     def __index__(self):
         return self.status_generate(200, "be welcome!")
 
-
     @api_post("/register")
     def register(self):
         posted_data = request.json
         # get of data's payload body
-        print(posted_data)
         username = posted_data.get("username")
         password = posted_data.get("password")
 
         hashed_password = bcrypt.hash(password)
-        print(hashed_password)
-        # users.insert({
-        #     "username": username,
-        #     "password": hashed_password,
-        #     "sentence": ""
-        # })
+        users.insert({
+            "username": username,
+            "password": hashed_password,
+            "sentence": "",
+            "tokens": 10
+        })
         return self.status_generate(200, "you successfully signed up for the API !!!")
-
 
     def status_generate(self, status: int, message: str):
         return {"status": status,
@@ -81,12 +77,52 @@ class Register(BottleResource):
 
 
 class Store(BottleResource):
-    pass
 
+    def verify_password(self, username, password):
+        hashed_password = users.find({
+            "username": username
+        })[0]["password"]
+        is_equal = True if bcrypt.verify(password, hashed_password) else False
+        return is_equal
+
+    def count_tokens(self, username):
+        tokens = users.find({"username": username})[0]["tokens"]
+        return tokens
+
+    def status_generate(self, status: int, message: str = None):
+        return {"status": status,
+                "message": message}
+
+    @api_post("/store")
+    def store(self):
+        posted_data = request.json
+        username = posted_data.get("username")
+        password = posted_data.get("password")
+        sentence = posted_data.get("sentence")
+
+        # verify math of password
+        correct_password = self.verify_password(username, password)
+        if not correct_password:
+            return self.status_generate(302)
+
+        number_tokens = self.count_tokens(username)
+        if number_tokens <= 0:
+            return self.status_generate(301)
+
+        # verify user has enough tokens
+        users.update({"username": username},
+                     {
+                         "$set": {
+                             "sentence": sentence,
+                             "tokens": (number_tokens - 1)
+                         }})
+
+        return self.status_generate(200, "store save successfully!!")
 
 
 if __name__ == '__main__':
     app = Bottle()
     # app.install(DemoResource())
     app.install(Register())
+    app.install(Store())
     app.run(host="0.0.0.0", port=8080, debug=True, reload=True)
