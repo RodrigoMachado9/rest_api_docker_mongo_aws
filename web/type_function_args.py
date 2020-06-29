573ea283197c6f89d6a15afcfded24be8eb81ddb
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-29 18:33:46 -0300

    test for fast_api

--- web/type_function_args.py
+++ web/type_function_args.py
@@ -6,4 +6,21 @@ def test(name: str):
     return 'hello %s' % name
 
 
-print(test('machado'))
+def test1(name: str):
+    return 'hello %s' % name
+
+
+# from fastapi import FastAPI
+# from uvicorn import run
+#
+# app = FastAPI()
+#
+#
+# @app.get('/fastapi')
+# def hello_world():
+#     return {"status": 200, "message": " helloworld"}
+#
+#
+#
+# if __name__ == '__main__':
+#     run(app)
